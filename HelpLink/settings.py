ed858c488c699438b39ee96a39b7fec730e06ebc
Sujit Hiwale <153737518+Sujit-Hiwale@users.noreply.github.com>
2025-11-19 10:36:53 +0530

    Update Django settings for database SSL configuration
    Added SSL configuration for database connection and updated the database connection settings.

--- HelpLink/settings.py
+++ HelpLink/settings.py
@@ -79,9 +79,15 @@ WSGI_APPLICATION = "HelpLink.wsgi.application"
 # Database (Render PostgreSQL)
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='postgresql://helplink_user:sujit%40helplink@dpg-ce12xxxxx-a.oregon-postgres.render.com:5432/helplink_db')
+        default=config(
+            'DATABASE_URL',
+            default='postgresql://helplink_user:sujit%40helplink@dpg-ce12xxxxx-a.oregon-postgres.render.com:5432/helplink_db'
+        ),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -112,6 +118,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
 
 
 
